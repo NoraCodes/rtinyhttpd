608c5a3b5e92d57c4135b911d56893f9f3f73522
SilverWingedSeraph <lfstindall@gmail.com>
2016-10-30 14:29:27 -0500

    Make mocking functions conform to PathBuf signature.

--- rtinyhttpd/src/resource.rs
+++ rtinyhttpd/src/resource.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::path::PathBuf;
 
 pub enum AcquisitionError {
     /// Used when a resource simply doesn't exist.
@@ -8,17 +8,17 @@ pub enum AcquisitionError {
 }
 
 /// A test acquisition strategy that doesn't actually check anything and always succeeds
-pub fn acquire_test_always_succeeding(path: &Path) -> Result<(), AcquisitionError> {
-    Ok(())
+pub fn acquire_test_always_succeeding(path: PathBuf) -> Result<PathBuf, AcquisitionError> {
+    Ok(path)
 }
 
 /// A test acquisition strategy that doesn't actually check anything and never finds a resource
-pub fn acquire_test_never_found(path: &Path) -> Result<(), AcquisitionError> {
+pub fn acquire_test_never_found(path: PathBuf) -> Result<PathBuf, AcquisitionError> {
     Err(AcquisitionError::NotFoundError)
 }
 
 /// A test acquisition strategy that doesn't actually check anything and never has permission to
 /// access a resource
-pub fn acquire_test_never_permitted(path: &Path) -> Result<(), AcquisitionError> {
+pub fn acquire_test_never_permitted(path: PathBuf) -> Result<PathBuf, AcquisitionError> {
     Err(AcquisitionError::NotPermittedToReadError)
 }
